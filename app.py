500b0aa1c1db359e42260418f94b2288aa13ceb1
YassinWael <yassinwaelmohamed84@gmail.com>
2025-07-06 15:51:45 +0300

    added post generating

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from icecream import ic
 from utils import *
 from json import loads,dumps
 import logging
+from os import startfile
 
 # Set up logging to both console and file
 log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -23,6 +24,9 @@ flask_logger = logging.getLogger('werkzeug')
 flask_logger.setLevel(logging.ERROR)
 
 
+POST_GENERATION_PROMPT = """Generate the text (caption) for a facebook page post. The page's niche, and extra information will be given to you.
+                            you also will be given an Image of which your text will be overlayed on in the post, so aim for 2-3 sentences max. Follow the extended niche
+"""
 
 
 load_dotenv()  # pull in .env vars
@@ -132,26 +136,35 @@ def pages():
 
 
 @app.route("/post/<page_id>", methods=["POST"])
-def post_to_page(page_id):
+def post_to_page_via_form(page_id):
+
+    message = request.form.get("message")
+    post_to_page(page_id,message, None)
+    return redirect(url_for('posts',page_id=page_id))
+
+
+def post_to_page(page_id,message,image_id):
+    logging.info(f"Posting to {page_id}")
+
     user_token = session.get("user_token")
     page_token = get_page_token(page_id, session)
-    message = request.form.get("message")
-
     if not user_token or not page_token or not message:
         return redirect("/login")
-
-    # image_id = upload_image_to_facebook(page_id, page_token, request.form.get("image_url"))
-
-    response = requests.post(
-        f"https://graph.facebook.com/v23.0/{page_id}/feed",
-        params={
+    params={
             "access_token": page_token,
             "message": message
-            # "attached_media":dumps([{"media_fbid":image_id}])
         }
+    
+    if image_id:
+        params['attached_media'] = dumps([{"media_fbid":image_id}])
+        logging.info(f"Image deteced and added: {image_id}")
+    response = requests.post(
+        f"https://graph.facebook.com/v23.0/{page_id}/feed",
+        params=params
     )
-    ic(response.status_code)
-    return redirect(url_for('posts', page_id=page_id))
+    logging.info(f"response: {response.json()}")
+    ic("Posting",response.status_code)
+    return response.json()
 
 
 @app.route('/posts/<page_id>')
@@ -184,23 +197,34 @@ def posts(page_id):
     list_of_post_ids = [post['id'] for post in posts_data]
     ic(posts_data,list_of_post_ids)
     print("----------------------------------")
+
     if list_of_post_ids: # make sure at least one post exists
         image = session.get(f"image_{page_id}")
         posts_niche = session.get(f"posts_niche_{page_id}")
         if not posts_niche or session.get('post_ids',[]) != list_of_post_ids: # new post or something similar
             niche_info = query_gemini(
-                prompt="Identify the primary niche of the Facebook page in two words. Then, provide a detailed breakdown of its extended niche, including tone, audience, content themes, and post types. This analysis will be used by another LLM to generate Facebook posts, so ensure it's clear, specific, and highly descriptive.",
+                prompt="Identify the primary niche of the Facebook page in two words. Then, provide a detailed breakdown of its extended niche, including tone, audience, content themes, and post types. 6 SENTENCES MAX (50 words) HARD LIMIT FOR THE EXTENDED NICHE DO NOT GO MORE. This analysis will be used by another LLM to generate Facebook posts, so ensure it's clear, and specific. for the last thing provide image_niche which should be optimized (two words max, general) to get the best image from an unsplash.com search",
                 text=str(posts_data),
-                scheme={'niche': None,'extended_niche':None}
+                scheme={"niche": None,"extended_niche":"more information, string only nothing extra.","image_niche":"2 words to search for the image, general"}
             )
+            ic(niche_info)
             posts_niche = niche_info.get('niche', 'General')
-            image = get_image_from_unsplash(posts_niche)
+            extended_niche = niche_info.get('extended_niche','General')
+            image_niche = niche_info.get('image_niche')
+            logging.info(f"Extended niche: {extended_niche}")
+            logging.info(f"image niche: {image_niche}")
+            image = get_image_from_unsplash(image_niche)
+
+            
+            session[f'extended_niche_{page_id}'] = extended_niche
             session[f"posts_niche_{page_id}"] = posts_niche
+            session[f'image_niche_{page_id}'] = image_niche
             session[f'image_{page_id}']= image
         else:
             ic(f"Using cached niche for {page_id}: {posts_niche}")
+
         if not image:
-            image = get_image_from_unsplash(posts_niche)
+            image = get_image_from_unsplash(image_niche)
             session[f'image_{page_id}']= image
 
 
@@ -210,6 +234,30 @@ def posts(page_id):
 
 @app.route('/generate_posts/<page_id>', methods=["POST"])
 def generate_posts(page_id):
-    pass
+    niche = session[f'posts_niche_{page_id}']
+    extended_niche = session[f'extended_niche_{page_id}']
+    image_niche = session[f'image_niche_{page_id}']
+
+
+    image_link = session.get(f'image_{page_id}',get_image_from_unsplash(image_niche))
+    img_data = requests.get(image_link) 
+    img_path = "image.png"
+
+    with open(img_path,"wb") as f: # downloading the unsplash image to local
+        f.write(img_data.content)
+
+    image_caption = query_gemini(prompt=POST_GENERATION_PROMPT,text=f"{niche,extended_niche}", image_path=img_path,scheme={"caption":None}).get("caption")
+    output_path = add_text_to_image(img_path,image_caption)
+
+    logging.info(f"image_caption: {image_caption}")
+    # uploading the final image since facebook doesn't do local urls
+    image_to_post_link = upload_image(output_path)
+    logging.info(f"image to post link: {image_to_post_link}")
+
+    
+    image_id = upload_image_to_facebook(page_id, get_page_token(page_id, session),image_to_post_link)
+    post_to_page(page_id,image_caption,image_id)
+
+    return redirect(request.referrer)
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from icecream import ic
 import requests
 import google.generativeai as genai
@@ -12,8 +13,11 @@ load_dotenv()
 
 genai_api_key = environ.get("genai_api_key")
 unsplash_key = environ.get("unsplash_access_key")
-
+image_upload_key = environ.get("image_upload_key")
+if not all([genai_api_key,unsplash_key,image_upload_key]):
+    logging.warning(f"One of the api keys is empty: {genai_api_key,unsplash_key,image_upload_key}")
 def upload_image_to_facebook(page_id, page_token,image_url):
+    logging.info(f"Uploading: {image_url}...")
     upload = requests.post(
         f"https://graph.facebook.com/v23.0/{page_id}/photos",
         params= {
@@ -47,23 +51,32 @@ def get_image_from_unsplash(query="fancy black"):
     if response.status_code == 200:
         print(response.json()['results'][0]['urls']['regular'])
         image_url = response.json()['results'][0]['urls']['regular']
-        ic(f"Image URL from Unsplash: {image_url}")
+        logging.info(f"Image URL from Unsplash: {image_url}")
         return image_url
     else:
         ic(f"Error fetching image from Unsplash: {response.status_code} - {response.text}")
         return None
 
-def query_gemini(prompt,text="",scheme=""):
+def query_gemini(prompt,text="",image_path="",scheme=""):
     genai.configure(api_key=genai_api_key)
     model = genai.GenerativeModel("gemini-2.0-flash")
-    JSON_ONLY = f"ONLY RETURN THE RESULT AS JSON, Make sure the json will NOT raise a JSONDecodeError when loaded with json.loads() in Python. You should make sure your json doesn't violate the scheme: {str(scheme)}, if unable to fill a value in the scheme simply leave it as None"
+    JSON_ONLY = f"ONLY RETURN THE RESULT AS JSON, Make sure the json will NOT raise a JSONDecodeError when loaded with json.loads() in Python. DOUBLE QUOTES You should make sure your json doesn't violate the scheme: {str(scheme)}, if unable to fill a value in the scheme simply leave it as None"
     try:
-        response = model.generate_content(f"{prompt}, {JSON_ONLY}:  {text}")
-        json_content = response.text.lstrip("```json\n").rstrip("```")
+        response = model.generate_content([f"{prompt}, {JSON_ONLY}:  {text}",Image.open(image_path)] if image_path else f"{prompt}, {JSON_ONLY}:  {text}")
+        raw_text = response.text
+        match = re.search(r"\{.*\}",raw_text,re.DOTALL)
+        if not match:
+            raise ValueError("No JSON found in response")
+        json_content = match.group(0)
+
         print(json_content)
+        logging.info(f"Received response from gemini: {json_content}")
         ic(loads(json_content))
+        if isinstance(json_content,list):
+            json_content = json_content[0]
         return loads(json_content)
     except Exception as e:
+        logging.error(f"Error with gemini api: {e}")
         ic(f"Error in query_gemini: {e}")
         return scheme if isinstance(scheme, dict) else {}
         
@@ -74,6 +87,7 @@ def get_page_token(page_id,session):
     Get the page access token for a given page ID.
     This is used to post to the page.
     """
+    logging.info(f"Getting token for: {page_id}")
     user_token = session.get("user_token")
     if not user_token:
         return None
@@ -86,12 +100,26 @@ def get_page_token(page_id,session):
     )
     if response.status_code == 200:
         ic(response.json())
+        logging.info(f"Token Response: {response.json()}")
         return response.json().get("access_token")
     return None
 
 
 
 def add_text_to_image(image_path, text="Money is life.", output_path="output.png"):
+    """
+    Add a glass-like effect and a blurred background to a given image, with
+    a message on top of it. The text is wrapped and its size is adjusted to fit
+    within the glass area. The output image is saved to a file.
+
+    Args:
+        image_path (str): path to the input image
+        text (str, optional): text to display on top of the image. Defaults to "Money is life."
+        output_path (str, optional): path to the output image. Defaults to "output.png"
+
+    Returns:
+        str: path to the output image
+    """
     img = Image.open(image_path).convert("RGBA")
     width, height = img.size
 
@@ -162,5 +190,24 @@ def add_text_to_image(image_path, text="Money is life.", output_path="output.png
     return output_path
 
 
-def generate_post(niche):
-    post_content = query_gemini(prompt="Please generate a facebook post text content for the current niche")
+def upload_image(image_path):
+    """Uploads an image to imgbb.com and returns the image's display URL
+    
+    Args:
+        image_path (str): Path to the image file to upload.
+    
+    Returns:
+        str: The URL of the uploaded image.
+    """
+    logging.info(f"Uploading {image_path}...")
+    with open(image_path,"rb") as f:
+        response = requests.post(
+            url = "https://api.imgbb.com/1/upload",
+            params= {
+                "key":image_upload_key
+            },
+            files={
+                "image":f
+            }
+        )
+    return response.json()['data']['display_url']
